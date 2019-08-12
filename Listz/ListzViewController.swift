ea802ea5c191d6820eab7e79321f849d50b7a11b
Rajarshi Rahut <rajarshi.rahut@gmail.com>
2019-08-11 22:39:45 -0400

    First Version

---
+++ Listz/ListzViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ListzViewController.swift
+//  Listz
+//
+//  Created by Rajarshi Rahut on 8/11/19.
+//  Copyright © 2019 Rajarshi Rahut. All rights reserved.
+//
+
+import UIKit
+
+class ListzViewController: UITableViewController {
+
+    let itemArray = ["Apples","Oranges","Bananas"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    //MARK: - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    //MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Row \(indexPath.row), item \(itemArray[indexPath.row])")
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
