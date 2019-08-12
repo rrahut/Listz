836c68d5076a6108af5f67ab644d849c6b6ae228
Rajarshi Rahut <rajarshi.rahut@gmail.com>
2019-08-11 21:39:42 -0400

    Initial Commit

---
+++ Listz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Listz
+//
+//  Created by Rajarshi Rahut on 8/11/19.
+//  Copyright © 2019 Rajarshi Rahut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
