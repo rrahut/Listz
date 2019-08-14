61a7995f854cb36830ce6ecb8f12617a85c94181
Rajarshi Rahut <rajarshi.rahut@gmail.com>
2019-08-14 19:01:42 -0400

    Persisting data using Codable protocol and Plist

--- Listz/Controllers/ListzViewController.swift
+++ Listz/Controllers/ListzViewController.swift
@@ -11,26 +11,14 @@ import UIKit
 class ListzViewController: UITableViewController {
 
     var itemArray = [Item]()
-    let defaults = UserDefaults.standard
-    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newItem1 = Item()
-        newItem1.title = "Apples"
-        itemArray.append(newItem1)
+        print(dataFilePath!)
         
-        let newItem2 = Item()
-        newItem2.title = "Oranges"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Bananas"
-        itemArray.append(newItem3)
-        
-        if let items = defaults.array(forKey: "ListzItemArray") as? [Item] {
-            itemArray = items
-        }
+        loadItems()
         
     }
 
@@ -55,7 +43,7 @@ class ListzViewController: UITableViewController {
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        tableView.reloadData()
+        saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -73,9 +61,9 @@ class ListzViewController: UITableViewController {
             let newItem = Item()
             newItem.title = textField.text!
             self.itemArray.append(newItem)
-            //self.defaults.set(self.itemArray, forKey: "ListzItemArray")
-            self.defaults.set(self.itemArray, forKey: "ListzItemArray")
-            self.tableView.reloadData()
+
+            self.saveItems()
+
         }
         
         let addItemPopupActionCancel = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
@@ -94,5 +82,29 @@ class ListzViewController: UITableViewController {
         
     }
     
+    //MARK: - Model manipulation Methods
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array! \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func loadItems(){
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error reading Items.plist file: \(error)")
+            }
+        }
+    }
+    
 }
-
